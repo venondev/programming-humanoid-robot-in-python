fb0e4f590ed540cfb48d55b3538d6f2f8affc156
venondev <venondesign@gmail.com>
2022-06-16 14:53:25 +0200

    rpc first

--- distributed_computing/agent_client.py
+++ distributed_computing/agent_client.py
@@ -6,10 +6,13 @@
  * Hints: [threading](https://docs.python.org/2/library/threading.html) may be needed for monitoring if the task is done
 '''
 
+from concurrent.futures import thread
+import threading
+import requests
 import weakref
 
 class PostHandler(object):
-    '''the post hander wraps function to be excuted in paralle
+    ''' the post hander wraps function to be executed in parallel
     '''
     def __init__(self, obj):
         self.proxy = weakref.proxy(obj)
@@ -22,6 +25,22 @@ class PostHandler(object):
         '''non-blocking call of ClientAgent.set_transform'''
         # YOUR CODE HERE
 
+class RPCRequest():
+
+    def __init__(self, host) -> None:
+        self.host = host
+
+    def __getattr__(self, param):
+        def start_req(*args, **kwargs):
+            return requests.post(self.host, json={
+                "method": param,
+                "params": args,
+                "jsonrpc": "2.0",
+                "id": 0,
+            }).json()["result"]
+
+        return start_req
+
 
 class ClientAgent(object):
     '''ClientAgent request RPC service from remote server
@@ -29,38 +48,48 @@ class ClientAgent(object):
     # YOUR CODE HERE
     def __init__(self):
         self.post = PostHandler(self)
+        self.rpc = RPCRequest("http://localhost:4000")
     
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
+        return self.rpc.get_angle(joint_name)
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
+        self.rpc.set_angle(joint_name, angle)
 
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
+        return self.rpc.get_posture()
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
         # YOUR CODE HERE
+        self.rpc.execute_keyframes(keyframes)
 
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
+        return self.rpc.get_transform(name)
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
+        return self.rpc.set_transform(effector_name, transform)
 
 if __name__ == '__main__':
-    agent = ClientAgent()
-    # TEST CODE HERE
+    # agent = ClientAgent()
+    # # TEST CODE HERE
+
+    rpc = RPCRequest("http://localhost:4000")
+    print(rpc.get_angle("HeadYaw"))
 
 
